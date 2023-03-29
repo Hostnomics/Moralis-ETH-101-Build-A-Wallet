585ae492d69c9eb0d2332123e5206ddc1f394731
Hostnomics <hostnomics@gmail.com>
2023-03-28 23:21:24 -0400

    Added constructor to set message upon deploy

--- contracts/helloWorld.sol
+++ contracts/helloWorld.sol
@@ -3,8 +3,16 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract helloWorld {
+    //state variables
+    string message;
+
+    //constructors - set initial state
+    constructor(string memory _message) {
+        message = _message; //set by deployer of contract
+    }
+
     function hello() public returns (string memory) {
-        string memory message = "Hello World";
+        // string memory message = "Hello World";
         return message;
     }
 }
