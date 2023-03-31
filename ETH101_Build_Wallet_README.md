0176921beca54b65f3c53f65c5749aa77ef75ca0
Hostnomics <hostnomics@gmail.com>
2023-03-31 18:20:25 -0400

    Updated 1.0_helloWorld.sol with a few variable types and global vs local scope example

--- ETH101_Build_Wallet_README.md
+++ ETH101_Build_Wallet_README.md
@@ -2,3 +2,9 @@
 
 1. Constructors [_See Constructors here_](https://academy.moralis.io/lessons/constructors-2).
 2. Structs [_See Structs here_](https://academy.moralis.io/lessons/structs-3).
+3. Mappings
+4. Visibility
+5. Error Handling
+   - require
+   - assert
+6.
--- contracts/1.0_helloWorld.sol
+++ contracts/1.0_helloWorld.sol
@@ -4,6 +4,12 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract helloWorld {
     //state variables
+    //Examples:
+    int a = -1; // signed integer can be negative
+    uint aa = 2; // unsigned integer, only positive (>=0)
+    address d = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2; //unique data type to solidity
+
+    //use this one
     string message;
 
     //constructors - set initial state
@@ -20,6 +26,12 @@ contract helloWorld {
     function helloPure() public pure returns (string memory) {
         // string memory message = "Hello World";
         // NO interaction with any other part of this contract:
-        return "PURE predefined string output here";
+
+        // https://academy.moralis.io/lessons/variables-scope - (2:50)
+        string
+            memory localMessage = "Local function scope variables must have data location type specified. Memory most common.";
+
+        // return "PURE predefined string output here";
+        return localMessage;
     }
 }
