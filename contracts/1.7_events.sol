3b35de0914ddb24d8738e6c8ec5f91e112700d08
Hostnomics <hostnomics@gmail.com>
2023-04-02 23:59:59 -0400

    Added transferCompleted event in 1.7_events.sol

--- contracts/1.7_events.sol
+++ contracts/1.7_events.sol
@@ -10,11 +10,18 @@ contract Bank {
     address owner;
 
     //Added Event (2:02): https://academy.moralis.io/lessons/events-3 - triggers when addBalance fn used
-    event balanceAdded(uint amount, address depositedTo);
+    // event balanceAdded(uint amount, address depositedTo);
     //(5:09) Covers "indexed" - we can use the indexed parameters to search/query for those events
-    event balanceAdded(uint indexed amount, address indexed depositedTo);
+    // event balanceAdded(uint indexed amount, address indexed depositedTo);
     //(5:30) - Don't need to index amount b/c we won't search for that, but we will search for depositedTo addy
     event balanceAdded(uint amount, address indexed depositedTo);
+
+    //Added transfer event
+    event transferCompleted(
+        address indexed from,
+        address indexed to,
+        uint amount
+    );
 
     modifier onlyOwner() {
         require(
@@ -46,6 +53,10 @@ contract Bank {
 
         uint previousSenderBalance = balance[msg.sender]; //ex 1000
         _transfer(msg.sender, recipient, amount); //ex send 750
+
+        //Added transferCompleted Event:
+        emit transferCompleted(msg.sender, recipient, amount);
+
         assert(balance[msg.sender] == previousSenderBalance - amount); //ex => 250 = 250 (1000 - 750)
         //event logs and further checks.
     }
@@ -53,61 +64,5 @@ contract Bank {
     function _transfer(address from, address to, uint amount) private {
         balance[from] -= amount; // balance[msg.sender] -= amount;
         balance[to] += amount; // balance[recipient] += amount;
-    }
-}
-
-//***************************** LATER VERSION BELOW **************************************** */
-
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.8.2 <0.9.0;
-
-contract Bank {
-    mapping(address => uint) balance;
-
-    address owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    // event balanceAdded(uint amount, address indexed depositedTo);
-    event depositDone(uint amount, address indexed depositedTo);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _; //run the function
-    }
-
-    //Updated (2:00) https://academy.moralis.io/lessons/payable-functions-3
-    // function addBalance(uint _toAdd) public returns (uint){
-    function deposit() public payable returns (uint) {
-        // require(msg.sender == owner, "Sorry, you are not the Owner/deployer of this contract");
-        // balance[msg.sender] += _toAdd;
-        balance[msg.sender] += msg.value;
-        // emit balanceAdded(_toAdd, msg.sender);
-        emit depositDone(msg.value, msg.sender);
-        return balance[msg.sender];
-    }
-
-    function getBalance() public view returns (uint) {
-        return balance[msg.sender];
-    }
-
-    function transfer(address recipient, uint amount) public {
-        require(balance[msg.sender] >= amount, "Not enough ETH brah!");
-        require(msg.sender != recipient, "Can't send ETH to yourself!");
-
-        uint previousSenderBalance = balance[msg.sender]; //ex 1000
-
-        _transfer(msg.sender, recipient, amount); //ex send 750
-        assert(balance[msg.sender] == previousSenderBalance - amount); //ex => 250 = 250 (1000 - 750)
-
-        //event logs and further checks.
-    }
-
-    function _transfer(address from, address to, uint amount) private {
-        balance[from] -= amount;
-        balance[to] += amount;
     }
 }
